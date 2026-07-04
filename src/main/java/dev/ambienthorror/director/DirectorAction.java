540a916e387536229217570957f58c96ad510fae
nhatxsmb1-create <nhatxsmb1@gmail.com>
2026-07-04 13:44:38 +0700

    Create DirectorAction.java

---
+++ src/main/java/dev/ambienthorror/director/DirectorAction.java
@@ -0,0 +1,13 @@
+package dev.ambienthorror.director;
+
+public enum DirectorAction {
+    NONE,
+    AMBIENT_SOUND,
+    SHADOW_FLICKER,
+    SHADOW_PERIPHERAL,
+    SHADOW_BEHIND,
+    RADIO_CORRUPTED,
+    SANITY_DRAIN,
+    THE_MAN_HINT,
+    WORLD_EVENT
+}
